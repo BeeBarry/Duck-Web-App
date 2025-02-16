0c631d87a49c38c9bd611c4d6919a9fc456a749e
BeeBarry <barry.namdari@gmail.com>
2025-02-16 09:55:20 +0100

    Quote class added

---
+++ Duck.Core/Models/Quote.cs
@@ -0,0 +1,11 @@
+namespace Duck.Core.Models;
+
+public class Quote
+{
+    public int QuoteId { get; set; }
+    public required string Content { get; set; }
+    public QuoteType Type { get; set; }
+    public int DuckId { get; set; }
+    public Duck Duck { get; set; } = null!;
+
+}
