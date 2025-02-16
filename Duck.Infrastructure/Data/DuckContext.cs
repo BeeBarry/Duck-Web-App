1b05e3ff13307f67f687e772899c645bcd09ef37
BeeBarry <barry.namdari@gmail.com>
2025-02-16 09:56:17 +0100

    Duckcontext added

--- Duck.Infrastructure/Data/DuckContext.cs
+++ Duck.Infrastructure/Data/DuckContext.cs
@@ -1,6 +1,28 @@
+using Duck.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Duck.Infrastructure.Data;
 
-public class DuckContext
+public class DuckContext : DbContext
 {
-    
+    public DuckContext(DbContextOptions<DuckContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Core.Models.Duck> Ducks => Set<Core.Models.Duck>();
+    public DbSet<Quote> Quotes => Set<Quote>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Konfigurerar relationen mellan Duck och Quotes
+        modelBuilder.Entity<Core.Models.Duck>()
+            .HasMany(d => d.Quotes)
+            .WithOne(q => q.Duck)
+            .HasForeignKey(q => q.DuckId);
+
+        // Konfigurerar hur QuoteType ska sparas i databasen
+        modelBuilder.Entity<Quote>()
+            .Property(q => q.Type)
+            .HasConversion<String>();
+    }
 }
