b00720ad6919b289dd4b3cac0cd041ed4ac0f233
BeeBarry <barry.namdari@gmail.com>
2025-02-16 09:57:13 +0100

    Added necessary code to Program.cs

--- Duck.Api/Program.cs
+++ Duck.Api/Program.cs
@@ -1,9 +1,27 @@
+using Duck.Core.Interfaces;
+using Duck.Core.Models;
+using Duck.Infrastructure.Data;
+using Duck.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Registrerar services
+builder.Services.AddDbContext<DuckContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IDuckRepository, DuckRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+
+{
+    options.AddPolicy("AllowLocal",
+        builder => builder
+            .WithOrigins("http://localhost:3000") // Om frontend körs på port 3000
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 
 var app = builder.Build();
 
@@ -15,30 +33,29 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowLocal");
 
-var summaries = new[]
+// Hämta alla ankor
+app.MapGet("/api/ducks", async (IDuckRepository repository) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var ducks = await repository.GetAllAsync();
+    return Results.Ok(ducks);
+});
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapGet("/api/ducks/{duckId}/quotes/random", async (
+    IDuckRepository repository,
+    int duckId,
+    QuoteType type) =>
+{
+    var quote = await repository.GetRandomQuoteByTypeAsync(duckId, type);
+
+    if (quote == null)
+        return Results.NotFound($" No quote found for duck {duckId} of typ {type}");
+
+    return Results.Ok(quote);
+
+});
+
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
