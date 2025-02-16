4b92413f41074aa13d663cdf32327e799c236ed7
BeeBarry <barry.namdari@gmail.com>
2025-02-16 15:29:00 +0100

    QuoteDto.cs added

---
+++ Duck.Core/DTOs/Quote/QuoteDto.cs
@@ -0,0 +1,10 @@
+using Duck.Core.Models;
+
+namespace Duck.Core.DTOs.Quote;
+
+public class QuoteDto
+{
+    public int Id { get; set; }
+    public required string Content { get; set; }
+    public QuoteType Type { get; set; }
+}
