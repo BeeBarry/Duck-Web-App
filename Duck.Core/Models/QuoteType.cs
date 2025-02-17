3a6e59124c15dd30ed5e4d97b81916f9517a37f9
BeeBarry <barry.namdari@gmail.com>
2025-02-17 20:51:24 +0100

    added Json Enum converter

--- Duck.Core/Models/QuoteType.cs
+++ Duck.Core/Models/QuoteType.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Duck.Core.Models;
 
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum QuoteType
 {
-    Wise,
-    Comical,
-    Dark
+    Wise = 0,
+    Comical = 1,
+    Dark = 2
 }
