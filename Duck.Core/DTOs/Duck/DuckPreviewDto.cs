fcc7b0c4e9be1d8956e9a1970d00d060a6ccbf16
BeeBarry <barry.namdari@gmail.com>
2025-02-17 20:49:56 +0100

    DuckPreviewDto.cs added

---
+++ Duck.Core/DTOs/Duck/DuckPreviewDto.cs
@@ -0,0 +1,10 @@
+namespace Duck.Core.DTOs.Duck;
+
+public class DuckPreviewDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public string Personality { get; set; }
+    public string Motto { get; set; }
+}
