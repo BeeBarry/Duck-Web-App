8200ba1ef725169c6933090adbf87285eb12a230
BeeBarry <barry.namdari@gmail.com>
2025-02-17 20:50:56 +0100

    fixed typo

--- Duck.Core/DTOs/Duck/UpdateDuckDto.cs
+++ Duck.Core/DTOs/Duck/UpdateDuckDto.cs
@@ -3,7 +3,7 @@ namespace Duck.Core.DTOs.Duck;
 public class UpdateDuckDto
 {
     public required string? Name { get; set; }
-    public required string? specialty { get; set; }
+    public required string? Specialty { get; set; }
     public required string? Personality { get; set; }
     public required string? Motto { get; set; }
 }
