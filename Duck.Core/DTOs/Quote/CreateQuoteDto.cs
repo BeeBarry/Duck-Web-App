08228c6ad8c342696f33badd62bf2d0a4d8985a5
BeeBarry <barry.namdari@gmail.com>
2025-03-24 13:25:21 +0100

    added explanation property

--- Duck.Core/DTOs/Quote/CreateQuoteDto.cs
+++ Duck.Core/DTOs/Quote/CreateQuoteDto.cs
@@ -5,5 +5,7 @@ namespace Duck.Core.DTOs.Quote;
 public class CreateQuoteDto
 {
     public required string Content { get; set; }
+    
+    public string? Explanation { get; set; }
     public required QuoteType Type { get; set; }
 }
