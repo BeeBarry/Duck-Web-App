9b0033f412dc045589c2ab2b0590af9f663a793d
BeeBarry <barry.namdari@gmail.com>
2025-03-24 13:25:38 +0100

    added explanation property

--- Duck.Core/Models/Quote.cs
+++ Duck.Core/Models/Quote.cs
@@ -4,6 +4,8 @@ public class Quote
 {
     public int QuoteId { get; set; }
     public required string Content { get; set; }
+    
+    public string? Explanation { get; set; }
     public QuoteType Type { get; set; }
     public int DuckId { get; set; }
     public Duck Duck { get; set; } = null!;
